52f18c2368386702a11d00500a6373bbc0831fa0
Anh Tran <trantheanh1512000@gmail.com>
2021-02-10 23:24:38 +0700

    finished script, i don't think there are any bugs, might revist later if i think of anything i need to add this was made just so i could download stuff easier and to have sth on my github activity

--- grapplerDownload.py
+++ grapplerDownload.py
@@ -1,19 +1,32 @@
 from youtubesearchpython import VideosSearch
 from pytube import YouTube as PyTube
+from pytube.cli import on_progress
 
-grappler = input("Enter grappler name: ")
-print(grappler)
+def main():
+    grappler = input("Enter grappler name: ")
+    print(grappler)
 
-searchTerm = grappler + ' "vs"'
+    searchTerm = grappler + ' "vs"'
+    folderPath = '/mnt/c/Users/Bun/Videos/' + grappler
 
-videoSearch = VideosSearch(searchTerm, limit = 3)
-videoResults = videoSearch.result()["result"]
+    videoSearch = VideosSearch(searchTerm, limit = 2)
+    videoResults = videoSearch.result()["result"]
 
-links = []
+    links = []
 
-for video in videoResults:
-    youtubeStream = PyTube(video["link"])
-    print(youtubeStream.streams.filter(only_video=True).first())
+    for video in videoResults:
+        links.append(video["link"])
 
+    for url in links: 
+        try:
+            ytObject = PyTube(url, on_progress_callback=on_progress)
+            ytObject.streams.get_highest_resolution().download(folderPath)
+        except Exception as e:
+            print(e)
+            raise Exception('Exception occured')
 
+    
+
+if __name__ == "__main__":
+    main()
 
